29b23084991f01c3ee7d41daa4d35be14929692a
Ivan Mingolov <ivanmingolov@gmail.com>
2023-05-24 15:13:27 +0200

    Implement standard deviation

--- statistics_basics.py
+++ statistics_basics.py
@@ -3,12 +3,12 @@ import math
 import numpy
 from functools import reduce
 
-# Mean
+# Mean : Average of the data set
 def mean(*args):
     sum_of_args = sum(args)
     return sum_of_args / len(args)
 
-# Median
+# Median : Number at the center of the data set
 def median(*args):
     if len(args) % 2 == 1: # length is odd
         i = round(len(args) / 2) # Middle element
@@ -18,21 +18,23 @@ def median(*args):
         j = i - 1
         return (args[i] + args[j]) / 2
 
-# Mode
+# Mode : Number that occurs most often in the data set
 def mode(*args):
     values_count = {i: args.count(i) for i in args} # Dictionary where the keys are all different values and values are the count of each value
     max_count = max(values_count.values())
     max_values = [key for key, value in values_count.items() if value == max_count] # All values that occur 'max' number of times
     return max_values
 
-# Variance
+# Variance : How is the data spread around the mean
 def variance(*args):
     mean_value = mean(*args)
     difference_to_mean_squared = list(map(lambda x : (x - mean_value) ** 2, args)) # map to every element the difference to the mean and squared it
-    variance_value = reduce(lambda x, y : x + y, difference_to_mean_squared) / (len(args) - 1) # sum all difference_to_mean_squared divided by the length - 1
+    variance_value = reduce(lambda x, y : x + y, difference_to_mean_squared) / (len(args) - 1) # sum all difference_to_mean_squared values and divides by the length - 1
     return variance_value
 
-# Standard deviation
+# Standard deviation : Square root of the variance, because variance gives extra weight to outliers because of the squared values
+def standard_deviation(*args):
+    return math.sqrt(variance(*args))
 
 # Coefficient of variation
 
@@ -42,7 +44,6 @@ def variance(*args):
 
 
 # Main
-
 print(f'Mean of [1, 2, 3, 4, 5] is {mean(1, 2, 3, 4, 5)}')
 
 print (f'Median of [1, 2, 3, 4, 5] is {median(1, 2, 3, 4, 5)}')
@@ -51,3 +52,5 @@ print (f'Median of [1, 2, 3, 4, 5, 6] is {median(1, 2, 3, 4, 5, 6)}')
 print (f'Mode of [1, 2, 3, 4, 5, 5, 4] is {mode(1, 2, 3, 4, 5, 5, 4)}')
 
 print (f'Variance of [4, 6, 3, 5, 2] is {variance(4, 6, 3, 5, 2)}')
+
+print (f'Standard deviation of [4, 6, 3, 5, 2] is {standard_deviation(4, 6, 3, 5, 2)}')
